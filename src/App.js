9a88efe065d39a6823d3e558318b8ada25d22270
rcminerva2394 <rosebahan@gmail.com>
2022-05-26 22:58:07 +0800

    Fixing filter/footer bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   const [tasksList, setTasksList] = useState([]);
-  const [filterTasksList, setFilterTasksList] = useState(null);
+  const [filterTasksList, setFilterTasksList] = useState(tasksList);
+  const [isFiltering, setIsFiltering] = useState(false);
 
   useEffect(() => {
     const getTasksList = localStorage.getItem("tasksList");
@@ -21,6 +22,9 @@ const App = () => {
   useEffect(() => {
     const saveTasksList = JSON.stringify(tasksList);
     localStorage.setItem("tasksList", saveTasksList);
+
+    setIsFiltering(false);
+    console.log("no filtering");
   }, [tasksList]);
 
   const addTaskHandler = (newTask) => {
@@ -63,22 +67,46 @@ const App = () => {
   };
 
   const summaryHandler = (category) => {
-    if (category === "All") {
-      setFilterTasksList(tasksList);
-    } else if (category === "Active") {
-      const activeList = tasksList.filter((task) => task.completed === false);
-      setFilterTasksList(activeList);
-    } else if (category === "Completed") {
-      const completedList = tasksList.filter((task) => task.completed === true);
-      setFilterTasksList(completedList);
+    switch (category) {
+      case "All":
+        setFilterTasksList(tasksList);
+        break;
+      case "Active":
+        const activeList = tasksList.filter((task) => task.completed === false);
+        setFilterTasksList(activeList);
+        break;
+      case "Completed":
+        const completedList = tasksList.filter(
+          (task) => task.completed === true
+        );
+        setFilterTasksList(completedList);
+        break;
+      default:
+        break;
     }
   };
+
+  // if (category === "All") {
+  //   setFilterTasksList(tasksList);
+  // } else if (category === "Active") {
+  //   const activeList = tasksList.filter((task) => task.completed === false);
+  //   setFilterTasksList(activeList);
+  // } else if (category === "Completed") {
+  //   const completedList = tasksList.filter((task) => task.completed === true);
+  //   setFilterTasksList(completedList);
+  // }
+
+  useEffect(() => {
+    setIsFiltering(true);
+    console.log("filter")
+  }, [filterTasksList]);
+
   return (
     <div className="todo-form">
       <h2>My Simple Todo</h2>
       <AddTask onAddTask={addTaskHandler}></AddTask>
       <Tasklist
-        tasks={filterTasksList ? filterTasksList : tasksList}
+        tasks={isFiltering ? filterTasksList : tasksList}
         onDelTask={delTaskHandler}
         onCompleted={completedHandler}
         onEdit={editHandler}
